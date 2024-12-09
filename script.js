d0f64d4baecaa1d70293516c87219ec331fb5425
bbantier <65014410+bbantier@users.noreply.github.com>
2024-12-09 19:31:49 +0100

    test first approach for checking winner

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const Gameboard = (() => {
 })();
 
 const Player = (name, marker) => {
-  name, marker;
+  return { name, marker };
 };
 
-const game = () => {
+const Game = (() => {
   let playerOne, playerTwo, currentPlayer;
   let gameOver = false;
 
@@ -21,15 +21,63 @@ const game = () => {
     currentPlayer = playerOne;
     Gameboard.resetBoard();
     gameOver = false;
+    playTurn();
   };
+
+  const switchPlayer = () =>
+    (currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne);
 
   const playTurn = (index) => {
+    index = prompt() - 1;
     if (gameOver) return;
 
-    const board = Gameboard.getBoard();
-
-    if (board[index] !== "") {
+    if (Gameboard.getBoard()[index] === "") {
       Gameboard.placeMarker(index, currentPlayer.marker);
+      console.log(Gameboard.getBoard());
+    } else {
+      console.log("This cell's already been claimed!");
+      return;
+    }
+
+    const result = checkWin();
+    console.log(result);
+    if (result) {
+      console.log(currentPlayer.name + " wins!");
+      return;
+    } else {
+      switchPlayer();
+      playTurn();
     }
   };
-};
+
+  const checkWin = () => {
+    const board = Gameboard.getBoard();
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const winTests = [];
+    winCombos.forEach((combo) => {
+      const [a, b, c] = combo;
+      if (board[a] && board[b] === board[a] && board[c] === board[a]) {
+        winTests.push(true);
+      } else {
+        winTests.push(false);
+      }
+    });
+
+    if (winTests.includes(true)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  return { startGame };
+})();
