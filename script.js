a2b9a8a2fed1d5c0f617a992226512cd22cb803e
bbantier <65014410+bbantier@users.noreply.github.com>
2024-12-09 17:27:43 +0100

    start game logic

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-const gameboard = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
+const Gameboard = (() => {
+  const board = Array(9).fill("");
   const getBoard = () => board;
-  const placeMarker = (index, marker) => board[index] = marker;
+  const placeMarker = (index, marker) => (board[index] = marker);
   const resetBoard = () => board.fill("");
 
   return { getBoard, placeMarker, resetBoard };
 })();
 
-const player = (name, marker) => { name, marker };
+const Player = (name, marker) => {
+  name, marker;
+};
 
 const game = () => {
   let playerOne, playerTwo, currentPlayer;
   let gameOver = false;
 
-  const startGame = (nameOne, nameTwo) => {
-    gameboard.resetBoard();
-    playerOne = player(nameOne, "x");
-    playerTwo = player(nameTwo, "o");
+  const startGame = () => {
+    playerOne = Player("Player 1", "x");
+    playerTwo = Player("Player 2", "o");
     currentPlayer = playerOne;
+    Gameboard.resetBoard();
+    gameOver = false;
   };
-}
+
+  const playTurn = (index) => {
+    if (gameOver) return;
+
+    const board = Gameboard.getBoard();
+
+    if (board[index] !== "") {
+      Gameboard.placeMarker(index, currentPlayer.marker);
+    }
+  };
+};
